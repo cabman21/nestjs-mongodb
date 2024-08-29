9ce897e3f5a3f29b3c315d42254736ec4ecaeafd
김진해 <pj.cabman21@kalmate.net>
2024-08-29 13:59:02 +0900

    update CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: 'http://localhost:3000',
+    methods: 'GET, POST, PUT, PATCH, DELETE',
     credentials: true,
   });
   await app.listen(8080);

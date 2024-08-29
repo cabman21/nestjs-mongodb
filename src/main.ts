30e411a8e19cd8b4acb14e3592d44a59b4a7c08d
김진해 <pj.cabman21@kalmate.net>
2024-08-29 14:39:48 +0900

    add "Access-Control-Allow-Origin": "*"

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: 'GET, POST, PUT, PATCH, DELETE',
-    credentials: true,
-  });
+  app.enableCors();
   await app.listen(8080);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
